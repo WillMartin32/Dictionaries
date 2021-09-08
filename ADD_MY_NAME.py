d56b6f0051d95101d9958b27619524cb685a3055
William Martin <william_martin3@baylor.edu>
2021-09-08 17:34:37 -0500

    Pushing class work

---
+++ ADD_MY_NAME.py
@@ -0,0 +1,8 @@
+def main():
+
+    outfile = open('philosophers.txt', 'a')
+    outfile.write('William Martin\n')
+    
+    outfile.close()
+
+main()
---
+++ read_philosophers.py
@@ -0,0 +1,14 @@
+def main():
+
+    infile = open('philosophers.txt' , 'r')
+
+    file_contents = infile.read()
+
+    line1 = infile.readline().rstrip('\n')
+    line2 = infile.readline()
+    line3 = infile.readline()
+
+    print(line1)
+    print(line2.rstrip('\n'))
+    print(line3)
+    
